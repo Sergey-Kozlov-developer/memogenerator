98666a2231ec9c07c84ad5c127ca2d5644c41f29
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-05-10 20:29:53 +0300

    new

---
+++ lib/blocs/create_meme_bloc.dart
@@ -0,0 +1,81 @@
+import 'package:rxdart/rxdart.dart';
+import 'package:uuid/uuid.dart';
+import 'package:collection/collection.dart';
+
+class CreateMemeBloc {
+  // данные логического объекта, которые получаем в UI
+  // отображение текста в верхней части приложения
+  final memeTextSubject = BehaviorSubject<List<MemeText>>.seeded(<MemeText>[]);
+  // выделенный в данный момент memeText
+  final selectedMemeTextSubject = BehaviorSubject<MemeText?>.seeded(null);
+
+  // при нажатии добавить текст, будет создаваться текст на холсте
+  void addNewText() {
+    final newMemeText = MemeText.create();
+    memeTextSubject.add([...memeTextSubject.value, newMemeText]);
+    selectedMemeTextSubject.add(newMemeText);
+  }
+// получить список с текущими memeText и найти нужный и заменить его id
+  void changeMemeText(final String id, final String text) {
+
+    final copiedList = [...memeTextSubject.value];
+    final index = copiedList.indexWhere((memeText) => memeText.id == id);
+    if (index == -1) {
+      return;
+    }
+    copiedList.removeAt(index);
+    copiedList.insert(index, MemeText(id: id, text: text));
+    memeTextSubject.add(copiedList);
+  }
+
+// при нажатии на текст в поле, метод выделяет текст в поле ввода и дает редактировать его
+  void selectMemeText(final String id) {
+    final foundMemeText =
+        memeTextSubject.value.firstWhereOrNull((memeText) => memeText.id == id);
+    selectedMemeTextSubject.add(foundMemeText);
+  }
+
+  void deselectMemeText() {
+    selectedMemeTextSubject.add(null);
+  }
+
+  // выдает инфу содерж в этом subject
+  Stream<List<MemeText>> observeMemeTexts() => memeTextSubject
+      .distinct((prev, next) => ListEquality().equals(prev, next));
+
+  Stream<MemeText?> observeSelectedMemeText() =>
+      selectedMemeTextSubject.distinct();
+
+  void dispose() {
+    memeTextSubject.close();
+    selectedMemeTextSubject.close();
+  }
+}
+
+// создание уникального индефикатора содержащего текст нашей надписи
+class MemeText {
+  final String id;
+  final String text;
+
+  MemeText({required this.id, required this.text});
+
+  factory MemeText.create() {
+    return MemeText(id: Uuid().v4(), text: "");
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MemeText &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          text == other.text;
+
+  @override
+  int get hashCode => id.hashCode ^ text.hashCode;
+
+  @override
+  String toString() {
+    return 'MemeText{id: $id, text: $text}';
+  }
+}
---
+++ lib/blocs/main_bloc.dart
@@ -0,0 +1,8 @@
+class MainBloc {
+
+
+
+  void dispose() {
+
+  }
+}
