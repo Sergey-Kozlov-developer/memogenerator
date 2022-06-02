10658dd364195f1858fb443b20906c430acd3b66
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-06-02 08:38:07 +0300

    new

--- lib/domain/interactors/save_meme_interactor.dart
+++ lib/domain/interactors/save_meme_interactor.dart
@@ -4,6 +4,7 @@ import 'package:memogenerator/data/models/meme.dart';
 import 'package:memogenerator/data/models/text_with_position.dart';
 import 'package:memogenerator/data/repositories/memes_repository.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:collection/collection.dart';
 
 class SaveMemeInteractor {
   static SaveMemeInteractor? _instance;
@@ -27,14 +28,37 @@ class SaveMemeInteractor {
     final docsPath = await getApplicationDocumentsDirectory();
     // папка с мемами создаем ее
     final memePath = "${docsPath.absolute.path}${Platform.pathSeparator}memes";
-    await Directory(memePath).create(recursive: true);
+    final memesDirectory = Directory(memePath);
+    await memesDirectory.create(recursive: true);
+
+    // получение текущего списка с файлами
+    final currentFiles = memesDirectory.listSync();
+
     // imageName получение названия файлика
-    final imageName = imagePath.split(Platform.pathSeparator).last;
+    final imageName = _getFileNameByPath(imagePath);
+    // есть ли файл сейчас с таким же названием
+    final oldFileWithTheSameName = currentFiles.firstWhereOrNull(
+      (element) {
+        return _getFileNameByPath(element.path) == imageName && element is File;
+      },
+    );
     // fullImagePath новый путь
     final newImagePath = "$memePath${Platform.pathSeparator}$imageName";
+    // создаем новый файл
     final tempFile = File(imagePath);
-    // скопировали в новую папку
-    await tempFile.copy(newImagePath);
+    if (oldFileWithTheSameName == null) {
+      // скопировали в новую папку
+      await tempFile.copy(newImagePath);
+    } else {
+      // запрашиваем размер файла
+      final oldFileLength = await (oldFileWithTheSameName as File).length();
+      // сколько занимает места файл, который хотим скопировать
+      final newFileLength = await tempFile.length();
+      if (oldFileLength != newFileLength) {
+        // избавляемся от расширения файла
+
+      }
+    }
 
     final meme = Meme(
       id: id,
@@ -43,4 +67,7 @@ class SaveMemeInteractor {
     );
     return await MemesRepository.getInstance().addToMemes(meme);
   }
+
+  String _getFileNameByPath(String imagePath) =>
+      imagePath.split(Platform.pathSeparator).last;
 }
